8c7119b59b154ae297a180fb0f07e4ad5002dba2
basuka <reon5525@gmail.com>
2022-03-01 22:14:34 +0900

    Update EnemyObject.js Update EnemyObject.md

--- request/EnemyObject/EnemyObject.js
+++ request/EnemyObject/EnemyObject.js
@@ -31,10 +31,25 @@
  *
  *=====================================================================================================================================================
  *
- * @param objEnemyIds
- * @type enemy[]
+ * @param objEnemyInfos
+ * @type struct<objEnemyInfo>[]
+ * @text オブジェクトエネミー情報設定
+ * @desc オブジェクトにするエネミー情報の設定
+ */
+
+/*~struct~objEnemyInfo:ja
+ * @param objEnemyId
+ * @type enemy
  * @text オブジェクトエネミー
  * @desc オブジェクトにするエネミー
+ *
+ * @param screenZ
+ * @type number
+ * @min -9999
+ * @max 9999
+ * @default 0
+ * @text Z座標
+ * @desc エネミー画像のZ座標
  */
 
 
@@ -85,9 +100,40 @@
     //-----------------------------------------------------------------------------
     // Game_Enemy
     //-----------------------------------------------------------------------------
-    Game_Enemy.prototype.isEnemy = function() {
-        return !params.objEnemyIds.includes(this._enemyId);
+    const _Game_Enemy_InitMembers = Game_Enemy.prototype.initMembers;
+    Game_Enemy.prototype.initMembers = function() {
+        _Game_Enemy_InitMembers.apply(this, arguments);
+        this._objEnemy = false;
+        this._screenZ = 0;
     };
+
+    const _Game_Enemy_Setup = Game_Enemy.prototype.setup;
+    Game_Enemy.prototype.setup = function(enemyId, x, y) {
+        _Game_Enemy_Setup.apply(this, arguments);
+        this.setEnemyType(enemyId);
+    }
+
+    Game_Enemy.prototype.setEnemyType = function(enemyId) {
+        for (const objEnemyInfo of params.objEnemyInfos) {
+            if (objEnemyInfo.objEnemyId === enemyId) {
+                this._objEnemy = true;
+                break;
+            }
+        }
+    }
+
+    Game_Enemy.prototype.isEnemy = function() {
+        return !this._objEnemy;
+    };
+
+
+    Game_Enemy.prototype.setScreenZ = function(z) {
+        this._screenZ = z;
+    }
+
+    Game_Enemy.prototype.screenZ = function() {
+        return this._screenZ
+    }
 
 
     //-----------------------------------------------------------------------------
@@ -98,6 +144,20 @@
         _Game_Troop_Clear.apply(this, arguments);
         this._allMemberFlg = true;
     };
+
+    const _Game_Troop_Setup = Game_Troop.prototype.setup;
+    Game_Troop.prototype.setup = function(troopId) {
+        _Game_Troop_Setup.apply(this, arguments);
+        for (const enemy of this._enemies) {
+            for (const objEnemyInfo of params.objEnemyInfos) {
+                if (enemy.enemyId() === objEnemyInfo.objEnemyId) {
+                    const screenZ = objEnemyInfo.screenZ;
+                    enemy.setScreenZ(screenZ);
+                    break;
+                }
+            }
+        }
+    }
 
     const _Game_Troop_prototype_members = Game_Troop.prototype.members;
     Game_Troop.prototype.members = function() {
@@ -125,5 +185,34 @@
     Game_Troop.prototype.setAllMemberFlg = function(allMemberFlg) {
         this._allMemberFlg = allMemberFlg;
     }
+
+
+    //-----------------------------------------------------------------------------
+    // Spriteset_Battle
+    //-----------------------------------------------------------------------------
+    const _Spriteset_Battle_CompareEnemySprite = Spriteset_Battle.prototype.compareEnemySprite;
+    Spriteset_Battle.prototype.compareEnemySprite = function(a, b) {
+        if (a.z !== b.z) {
+            return a.z - b.z;
+        } else {
+            return _Spriteset_Battle_CompareEnemySprite.apply(this, arguments);
+        }
+    };
+
+
+    //-----------------------------------------------------------------------------
+    // Sprite_Enemy
+    //-----------------------------------------------------------------------------
+    const _Sprite_Enemy_InitMembers = Sprite_Enemy.prototype.initMembers;
+    Sprite_Enemy.prototype.initMembers = function() {
+        _Sprite_Enemy_InitMembers.apply(this, arguments);
+        this.z = 0;
+    };
+
+    const _Sprite_Enemy_SetBattler = Sprite_Enemy.prototype.setBattler;
+    Sprite_Enemy.prototype.setBattler = function(battler) {
+        _Sprite_Enemy_SetBattler.apply(this, arguments);
+        this.z = battler.screenZ();
+    };
 
 })();
